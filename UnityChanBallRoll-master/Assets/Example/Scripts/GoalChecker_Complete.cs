b5a93c745687a13e6782739aa767c44202795730
hinode000jp <takenoko000jp@gmail.com>
2019-11-06 01:07:47 +0900

    first commit

---
+++ UnityChanBallRoll-master/Assets/Example/Scripts/GoalChecker_Complete.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GoalChecker_Complete : MonoBehaviour
+{
+    public GameObject unityChan;
+    public AudioSource gameBgm;
+    public AudioSource goalBgm;
+    public Camera mainCam;
+    public Camera subCam;
+
+    public GameObject retryButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        retryButton.SetActive(false);
+        subCam.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+        //unityChan.transform.LookAt(Camera.main.transform);
+        unityChan.GetComponent<Animator>().SetTrigger("Goal");
+
+        mainCam.enabled = false;
+        subCam.enabled = true;
+
+        gameBgm.Stop();
+        goalBgm.Play();
+
+        retryButton.SetActive(true);
+
+    }
+
+    public void RetryStage()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+}
